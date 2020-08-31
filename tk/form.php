3a1d02ef571efd2b73f317a1c3c4fcb87bb0f3cc
Guilherme1-jpg <59875701+Guilherme1-jpg@users.noreply.github.com>
2020-08-31 15:39:59 -0300

    Update form.php

--- tk/form.php
+++ tk/form.php
@@ -12,9 +12,24 @@
 		$email = $_POST['email'];
 		$_POST['notification'] = (isset($_POST['notification'])) ?$_POST['notification']:null; //verificar se o usuario nao enviara sem valor
 
-		echo "Ola!, Um aluno se cadastrou! Segue os dados: ";
-		echo "Seu nome $name e email $email"; 
+		echo "\nOla!, Um aluno se cadastrou! Segue os dados: ";
+		echo "\nSeu nome $name \n";
+		echo "email $email \n";
+		
+		$socket = fsockopen('udp://pool.ntp.br', 123, $err_no, $err_str, 1);
 
+
+		if ($socket)
+		{
+   			if (fwrite($socket, chr(bindec('00'.sprintf('%03d', decbin(3)).'011')).str_repeat(chr(0x0), 39).pack('N', time()).pack("N", 0)))
+    		{
+        	stream_set_timeout($socket, 1);
+        	$unpack0 = unpack("N12", fread($socket, 48));
+        	echo date('d-m-Y H:i:s', $unpack0[7]);
+   		 }
+
+    	fclose($socket);
+		} 
 		//Peço desculpas, mas nao tenho conhecimento em cURL. 
 
 
